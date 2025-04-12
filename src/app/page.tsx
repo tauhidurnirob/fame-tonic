93c4bec5148956c9ebbfb461e20e35bdf35b760a
Tauhidur <tauhidur@consap.sg>
2025-04-12 13:17:07 +0800

    ui fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <Box sx={{ background: "#000000" }}>
+    <Box>
       <Banner />
       <Box maxWidth={"1240px"} mx={"auto"} px={3}>
         <Header />
